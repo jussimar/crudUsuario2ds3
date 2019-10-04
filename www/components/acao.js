9678594e839821c596bca516662ae21a92dd48fa
jussimar <jussimar@jussimarleal.com.br>
2019-10-05 07:17:47 +0900

    enviando dados ao webservice

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,3 +3,29 @@
 $(document).on("click","#listar",function(){
   $(location).attr("href","listar.html");
 });
+
+$(document).on("click","#salvar",function(){
+  var parametros = {
+        "nome": $("#nome").val(),
+        "senha": $("#senha").val(),
+        "email": $("#email").val()
+    };
+
+    $.ajax({
+        type:"post", //como enviar
+        url:"https://dominioappsjussa.000webhostapp.com/cadastra.php",//para onde enviar
+        data:parametros,//o que enviar
+        //se der certo
+        success: function(data){
+            navigator.notification.alert(data);
+            $("#nome").val("");
+            $("#senha").val("");
+            $("#email").val("");
+
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert("Erro ao cadastrar");
+        }
+    });    
+});
